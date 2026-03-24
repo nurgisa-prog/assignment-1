9bd57316a1a223a63996436c27a9b8ba188e75ae
TechHub <nurgisamaksuthan@gmail.com>
2026-03-24 14:29:38 +0500

    assignment 1

--- src/Main.java
+++ src/Main.java
@@ -34,7 +34,7 @@ public class Main {
     }
 
     // Task 2
-    static int sum(int[] arr, int n) {
+    static  int sum(int[] arr, int n) {
         if  (n == 0) return 0;
         return arr[n - 1] + sum(arr, n - 1);
     }
