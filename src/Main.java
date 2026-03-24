a36dfae8497b234e8223dc38273571854c8c8911
TechHub <nurgisamaksuthan@gmail.com>
2026-03-24 14:55:12 +0500

    assignment 1

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,8 @@ public class Main {
 
         // Task 2
         System.out.println("\nTask 2:");
-        int[] arr = {3, 2, 4, 1};
-        System.out.println("Average: " + average(arr, arr.length));
+        int[] arr1 = {3, 2, 4, 1};
+        System.out.println("Average: " + average(arr1, arr1.length));
 
         // Task 3
         System.out.println("\nTask 3:");
@@ -21,6 +21,34 @@ public class Main {
         // Task 4
         System.out.println("\nTask 4:");
         System.out.println("Factorial of 5: " + factorial(5));
+
+        // Task 5
+        System.out.println("\nTask 5:");
+        System.out.println(fib(5));
+
+        // Task 6
+        System.out.println("\nTask 6:");
+        System.out.println(power(2, 10));
+
+        // Task 7
+        System.out.println("\nTask 7:");
+        int[] arr2 = {1, 4, 6, 2};
+        reversePrint(arr2, arr2.length - 1);
+
+        // Task 8
+        System.out.println("\n\nTask 8:");
+        System.out.println(checkDigits("123456"));
+        System.out.println(checkDigits("123a12"));
+
+        // Task 9
+        System.out.println("\nTask 9:");
+        System.out.println(countChars("hello"));
+        System.out.println(countChars("recursion"));
+
+        // Task 10
+        System.out.println("\nTask 10:");
+        System.out.println(gcd(32, 48));
+        System.out.println(gcd(10, 7));
     }
 
     // Task 1
@@ -57,5 +85,44 @@ public class Main {
     static int factorial(int n) {
         if (n == 0 || n == 1) return 1;
         return n * factorial(n - 1);
+    }
+
+    // Task 5
+    static int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    // Task 6
+    static int power(int a, int n) {
+        if (n == 0) return 1;
+        return a * power(a, n - 1);
+    }
+
+    // Task 7
+    static void reversePrint(int[] arr, int index) {
+        if (index < 0) return;
+        System.out.print(arr[index] + " ");
+        reversePrint(arr, index - 1);
+    }
+
+    // Task 8
+    static String checkDigits(String s) {
+        if (s.length() == 0) return "Yes";
+        if (!Character.isDigit(s.charAt(0))) return "No";
+        return checkDigits(s.substring(1));
+    }
+
+    // Task 9
+    static int countChars(String s) {
+        if (s.equals("")) return 0;
+        return 1 + countChars(s.substring(1));
+    }
+
+    // Task 10
+    static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
     }
 }
