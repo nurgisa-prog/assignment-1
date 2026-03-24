6c7ae72ef43db0a601aacae90bb34a7972c546ff
TechHub <nurgisamaksuthan@gmail.com>
2026-03-24 14:23:25 +0500

    assignment 1

--- src/Main.java
+++ src/Main.java
@@ -41,7 +41,7 @@ public class Main {
 
     static double average(int[] arr, int n) {
         if (n == 0) return 0;
-        return  (double) sum(arr, n) / n;
+        return (double) sum(arr, n) / n;
     }
 
     // Task 3
@@ -56,6 +56,6 @@ public class Main {
     // Task 4
     static int factorial(int n) {
         if (n == 0 || n == 1) return 1;
-        return n * factorial(n - 1);
+         return n * factorial(n - 1);
     }
 }
